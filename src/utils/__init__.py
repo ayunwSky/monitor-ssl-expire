4330c8e797d79423eb2306be9427ad054f221416
ayunwSky <ayunwSky@gmail.com>
2023-05-22 09:44:42 +0800

    feat: 在包中的__init__.py中将项目根目录添加到sys.path

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -7,6 +7,10 @@
 # -*- Desc        :  None
 # *******************************************
 
+import os
+import sys
 
-
-
+# 把项目根目录加入到 sys.path 中
+root_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+if root_path not in sys.path:
+    sys.path.insert(0, root_path)
