774999cae779ddee8d1631fc9cbea46d4926bed6
ayunwSky <28972707+ayunwSky@users.noreply.github.com>
2023-05-26 15:31:00 +0800

    feat: 增加微信和email告警通道类型

--- src/utils/alertChannelType.py
+++ src/utils/alertChannelType.py
@@ -3,14 +3,139 @@
 
 import json
 import urllib3
+import smtplib
 import datetime
 import requests
 from requests.adapters import HTTPAdapter
+from email.utils import formataddr
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 from utils import genSign, settings
 from utils.customLogging import customLogger
 
 urllib3.disable_warnings()
+
+
+class WeiXin(object):
+    """
+      微信消息发送
+    """
+    headers = {"Content-Type": "application/json"}
+    req_message = {"errcode": 1, "errmsg": ""}
+
+    def __init__(self, corpid, corpsecret, agentid):
+        """
+        :param corpid:  企业id
+        :param corpsecret:  自定义应用secret
+        :param agentid: 自定义应用ID
+        """
+        self.corpid = corpid
+        self.corpsecret = corpsecret
+        self.agentid = agentid
+
+    def __access_token(self):
+        """
+        :return: access_token
+        """
+        try:
+            req = requests.get("https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={0}&corpsecret={1}".format(
+                self.corpid, self.corpsecret))
+            if req.json()["errcode"] != 0:
+                self.req_message["errcode"] = 0
+                self.req_message["errmsg"] = "获取access_token失败:" + str(req.json())
+            else:
+                self.req_message["errmsg"] = req.json()["access_token"]
+            return self.req_message
+        except Exception as e:
+            self.req_message["errcode"] = 0
+            self.req_message["errmsg"] = "获取access_token失败,监测你的网络是否正常"
+            return self.req_message
+
+    def sendmessage(self, touser, message):
+        """
+        :param touser: 发送给的用户,会@对应的人 支持多用户UserID1|UserID2|UserID3
+        :param message:  发送的消息
+        :return:  json
+        """
+        data = {
+            "touser": touser,
+            "msgtype": "text",
+            "agentid": self.agentid,
+            "text": {
+                "content": message
+            },
+            "safe": 0
+        }
+        access_token = self.__access_token()
+        if access_token["errcode"]:
+            try:
+                req = requests.post("https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token={}".format(
+                    access_token["errmsg"]),
+                                    data=json.dumps(data),
+                                    headers=self.headers,
+                                    timeout=10)
+
+                if req.json()["errcode"] == 0:
+                    self.req_message["errmsg"] = "发送成功"
+                else:
+                    self.req_message["errcode"] = 0
+                    self.req_message["errmsg"] = str(req.json())
+            except:
+                self.req_message["errcode"] = 0
+                self.req_message["errmsg"] = "发送失败,请求接口错误"
+        else:
+            return access_token
+        return self.req_message
+
+
+class Email(object):
+    """
+    邮件消息,默认QQ邮箱不用传递smtp参数
+    """
+    req_message = {"errcode": 1, "errmsg": ""}
+
+    def __init__(self, from_sender, from_pass, smtp="smtp.qq.com", smtp_port=465, smtp_ssl=True):
+        """
+        :param from_sender: 发件人
+        :param from_pass: 发件人密码
+        :param smtp: smtp地址,默认QQ邮箱
+        :param smtp_port: smtp端口
+        :param smtp_ssl: 是否启用ssl
+        """
+        self.from_sender = from_sender
+        self.from_pass = from_pass
+        self.smtp = smtp
+        self.smtp_port = smtp_port
+        self.smtp_ssl = smtp_ssl
+
+    def sendmessage(self, to_mail, title, message):
+        """
+        :param to_mail: 收件人
+        :param message: 消息类容
+        :return:
+        """
+        ret = True
+        try:
+            message = MIMEMultipart()
+            msg = MIMEText(message, 'plain', 'utf-8')
+            msg['From'] = formataddr([str(self.from_sender), self.from_sender])
+            msg['To'] = formataddr([str(to_mail), to_mail])
+            msg['Subject'] = title
+
+            if not self.smtp_ssl:
+                server = smtplib.SMTP(self.smtp, self.smtp_port)
+            else:
+                server = smtplib.SMTP_SSL(self.smtp, self.smtp_port)
+            server.login(self.from_sender, self.from_pass)
+            server.sendmail(self.from_sender, [
+                to_mail,
+            ], msg.as_string())
+            server.quit()
+        except Exception as e:
+            self.req_message["errcode"] = 0
+            self.req_message["errmsg"] = f"发送失败{e}"
+        return self.req_message
 
 
 class DinTalk(object):
